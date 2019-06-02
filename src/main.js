e0879ca0135852ed975fee284a1b1dba53d57c7b
sandalen <806403789@qq.com>
2019-06-02 14:43:32 +0800

    submit

---
+++ src/main.js
@@ -0,0 +1,15 @@
+// 入口文件
+console.log('hello')
+import Vue from 'vue'
+import { Header } from 'mint-ui'
+Vue.component(Header.name,Header)
+
+import './lib/mui/css/mui.min.css'
+
+import app from './App.vue'
+
+var vm = new Vue({
+	el: "#app",
+	render: c => c(app)
+	
+})
